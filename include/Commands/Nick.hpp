62550cf15b53b1c27567b28a2d84c161b0ea8376
Melanie Yanez <melanieyanez@gmail.com>
2024-09-23 07:36:00 +0200

    PASS OK

--- include/Commands/Nick.hpp
+++ include/Commands/Nick.hpp
@@ -12,13 +12,14 @@ namespace Commands
 		public:
 
 			Nick(std::vector<std::string> command_parts);
-			
+
 			virtual void 	execute(Client& client, Server& server);
 
 		protected:
 
 			std::string 	name;
-
+			bool			error;
+			std::string		errorMessage;
 	};
 }
 
--- src/Commands/Nick.cpp
+++ src/Commands/Nick.cpp
@@ -3,12 +3,35 @@
 Commands::Nick::Nick(std::vector<std::string> command_parts)
 {
 	if (command_parts.size() != 2)
-		throw ;
-
+	{
+		this->error = true;
+		this->errorMessage = "461 NICK :Wrong number of parameters";
+		return;
+	}
+	this->error = false;
 	this->name = command_parts[1];
 }
 
-void Commands::Nick::execute(Client& client, Server&)
+void Commands::Nick::execute(Client& client, Server& server)
 {
+	if (this->error)
+	{
+		client.sendBack(this->errorMessage, "client");
+		return;
+	}
+	if (!client.hasPass())
+	{
+		client.sendBack("451 NICK :You have not registered", "client");
+        return;
+	}
+	if (server.isNicknameConnected(name))
+	{
+     	client.sendBack("433 " + this->name + " :Nickname is already in use", "client");
+        return;
+	}
+	if (client.getIsAuthenticated())
+    {
+        client.sendBack(":" + client.getFullIdentifier() + " NICK :" + name);
+    }
 	client.setNickname(name);
 }
